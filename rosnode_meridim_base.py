e3ce245d8eebf49b0fe8c34a94e9c8fbe743d8d5
Ninagawa123 <ycanned@yahoo.co.jp>
2021-11-19 23:00:27 +0900

    Update rosnode_meridim_base.py
    checksum problem fixed

--- rosnode_meridim_base.py
+++ rosnode_meridim_base.py
@@ -5,21 +5,17 @@
 from re import I
 import rospy
 from sensor_msgs.msg import JointState
-
 import numpy as np
-
 import socket
 from contextlib import closing
 import struct
 import math
-
 import signal
 
-
-UDP_RESV_IP="192.168.xx.xx" #このPCのIPアドレス
+UDP_RESV_IP="192.168.xxx.xxx" #このPCのIPアドレス
 UDP_RESV_PORT=22222 #受信ポート
 
-UDP_SEND_IP="192.168.xx.xx" #送信先のIPアドレス
+UDP_SEND_IP="192.168.xxx.x" #送信先のIPアドレス
 UDP_SEND_PORT=22224 #送信ポート
 
 MSG_SIZE = 90
@@ -31,9 +27,7 @@ sock.bind((UDP_RESV_IP,UDP_RESV_PORT))
 data2 = []
 loop_count = 0
 error_count = 0
-
 global i
-
 
 while not rospy.is_shutdown():
     with closing(sock):
@@ -42,7 +36,6 @@ while not rospy.is_shutdown():
             r_bin_data,addr = sock.recvfrom(1472)
             sock.sendto(r_bin_data,(UDP_SEND_IP,UDP_SEND_PORT))
             r_short_data=struct.unpack('90h',r_bin_data)
-            
             print(r_short_data)
 
             checksum = np.array([0], dtype=np.int16)
@@ -51,9 +44,8 @@ while not rospy.is_shutdown():
             checksum[0] = ~checksum[0] & 0xffff
             print("[Calc] ",checksum[0])
             print("[Ans ] ",r_short_data[MSG_SIZE-1])
-
+            
             if checksum[0] == r_short_data[MSG_SIZE-1]:
-                
                 joint_pub = rospy.Publisher('joint_states', JointState, queue_size=10)
                 rospy.init_node('joint_state_publisher_meridim')
                 rate = rospy.Rate(500) # 500hz
@@ -66,16 +58,12 @@ while not rospy.is_shutdown():
                 js_meridim.effort = []        
                 joint_pub.publish(js_meridim)
                 rate.sleep() 
-
             else:
                 #print("DATA ERROR")
                 error_count += 1
-
             signal.signal(signal.SIGINT, signal.SIG_DFL)
             
-            print("COUNT:",loop_count," ERROR:",error_count," ErrorRatio:",'{:.02f}'.format(error_count/loop_count*100),"%")
-        
-    
+            print("COUNT:",loop_count," ERROR:",error_count," ErrorRate:",'{:.02f}'.format(error_count/loop_count*100),"%")
 
 #if __name__ == '__main__':
     #main()
