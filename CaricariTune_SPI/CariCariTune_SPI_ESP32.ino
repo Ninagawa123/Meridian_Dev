8914557cd86f586b5358b8aef4e3ac8a6f049a0f
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2022-01-29 12:03:24 +0900

    Update CariCariTune_SPI_ESP32.ino
    ショート型に対応
    チェックサムも完成形

--- CaricariTune_SPI/CariCariTune_SPI_ESP32.ino
+++ CaricariTune_SPI/CariCariTune_SPI_ESP32.ino
@@ -1,7 +1,10 @@
 // カリカリチューン動く！ Teensy4.0 - (SPI) - ESP32DevKitC 通信
-// ESP32用
-#include <ESP32DMASPISlave.h>
+// ESP32用　2022.01.29
+// short型に対応
+// チェックサムも上手くいっているこの式にすべてあわせること
 
+#include <ESP32DMASPISlave.h>
+#include <FastCRC.h>
 ESP32DMASPI::Slave slave;
 
 static const int MSG_SIZE = 90;
@@ -16,44 +19,44 @@ long errorcount = 0;
 //共用体の設定。共用体はたとえばデータをショートで格納し、バイト型として取り出せる
 typedef union
 {
-  short sval[MSG_SIZE];
-  uint8_t bval[MSG_BUFF];
+  short sval[MSG_SIZE + 2];
+  uint8_t bval[MSG_BUFF + 4];
 } UnionData;
-UnionData s_spi_meridim; //SPI受信用共用体のインスタンスを宣言
-UnionData r_spi_meridim; //SPI受信用共用体のインスタンスを宣言
-UnionData s_udp_meridim; //UDP送信用共用体のインスタンスを宣言
-UnionData r_udp_meridim; //UDP受信用共用体のインスタンスを宣言
+UnionData s_spi_meridim; //SPI受信用共用体
+UnionData r_spi_meridim; //SPI受信用共用体
+UnionData s_udp_meridim; //UDP送信用共用体
+UnionData r_udp_meridim; //UDP受信用共用体
 
 void setup()
 {
   Serial.begin(2000000);
-  delay(120);//シリアルの開始を待ち安定化させるためのディレイ（要調整）
-  Serial.println("SPI Slave Start.");//シリアルモニタの確認用。
+  delay(120);//シリアルの開始を待ち安定化させるためのディレイ（ほどよい）
+  Serial.println("SPI Slave Start.");//シリアルモニタの確認用
 
   // DMAバッファを使う設定　これを使うと一度に送受信できるデータ量を増やせる
-  s_spi_meridim_dma = slave.allocDMABuffer(MSG_BUFF + 4); //DMAバッファを使う
-  r_spi_meridim_dma = slave.allocDMABuffer(MSG_BUFF + 4); //DMAバッファを使う
+  s_spi_meridim_dma = slave.allocDMABuffer(MSG_BUFF + 4); //DMAバッファ設定 +4は取りこぼし対策
+  r_spi_meridim_dma = slave.allocDMABuffer(MSG_BUFF + 4); //DMAバッファ設定
 
   // 送受信バッファをリセット
-  memset(s_spi_meridim_dma, 0, MSG_BUFF + 4);
+  memset(s_spi_meridim_dma, 0, MSG_BUFF + 4);// +4は取りこぼし対策
   memset(r_spi_meridim_dma, 0, MSG_BUFF + 4);
 
-  //送信データを作成してセット
+  //初回の送信データを作成してセット
   checksum = 0;
-  for (int i = 0; i < MSG_SIZE - 1; i++) //配列の末尾以外をデータを入れる
+  for (int i = 0; i < MSG_SIZE - 1; i++) //配列の末尾以外にデータを入れる
   {
-    short rnd = random(-30000, 30000);
-    s_spi_meridim.sval[i] = rnd;
-    checksum += rnd; //チェックサムを加算
+    short rnd = random(-30000, 30000);//ここではランダムな配列を使う
+    s_spi_meridim.sval[i] = rnd;//配列にランダムなデータを入れる
+    checksum += int(s_spi_meridim.sval[i]); //チェックサムを加算
   }
-  s_spi_meridim.sval[MSG_SIZE - 1] = short(checksum & 0xFF ^ 0xFF); //データ末尾にチェックサムにする
+  s_spi_meridim.sval[MSG_SIZE - 1] = short(~checksum); //データ末尾にチェックサムを入れる
 
-  for (int i = 0; i < MSG_BUFF - 1 ; i++) { //受信データの転記
+  for (int i = 0; i < MSG_BUFF + 4; i++) { //送信データをDMAバッファに転記
     s_spi_meridim_dma[i] = s_spi_meridim.bval[i] ;
   }
 
   slave.setDataMode(SPI_MODE3);
-  slave.setMaxTransferSize(MSG_BUFF);
+  slave.setMaxTransferSize(MSG_BUFF + 4);
   slave.setDMAChannel(2); // 専用メモリの割り当て（1か2のみ)
   slave.setQueueSize(1); // キューサイズ　とりあえず1
   // HSPI(SPI2) のデフォルトピン番号は CS: 15, CLK: 14, MOSI: 13, MISO: 12
@@ -63,7 +66,7 @@ void setup()
 void loop()
 {
 
-  //このゾーンは高速で回る//
+  //この行にコードを記入すると高速で回るので注意//
 
   // キューが送信済みであればセットされた送信データを送信する。
   if (slave.remained() == 0) {
@@ -74,49 +77,36 @@ void loop()
   // バッファも自動更新される
   while (slave.available())
   {
-    slave.pop();//トランザクションを終了するコマンドらしい
+    slave.pop();//トランザクションを終了するコマンド？
 
-    for (int i = 0; i < MSG_BUFF - 1 ; i++) { //受信データの転記
+    for (int i = 0; i < MSG_BUFF ; i++) { //受信データの転記
       r_spi_meridim.bval[i] = int(r_spi_meridim_dma[i]);
     }
-
-    for (int i = 0; i < MSG_SIZE ; i++) { //受信データの転記
-      //Serial.print(r_spi_meridim.sval[i]);
-      //Serial.print(",");
-    }
-    //Serial.println();
-
 
     //受信データのチェックサム確認
     checksum = 0;
     for (int i = 0; i < MSG_SIZE - 1 ; i++) { //受信データの末尾-1番までの値を合計
       checksum += int(r_spi_meridim.sval[i]);
     }
-    checksum = (checksum ^ 0xff) & 0xff; //合計値を反転し、下位2バイトを取得
-    //Serial.print(" cksum: "); Serial.println(uint8_t (r_spi_meridim_dma[MSG_BUFF - 1]));
-
-    if (checksum == short(r_spi_meridim.sval[MSG_SIZE - 1])) {
-      //Serial.print("   OK!: "); Serial.println(short (checksum));
+    checksum = short(~checksum);//チェックサムを計算
+    if (checksum == r_spi_meridim.sval[MSG_SIZE - 1]) {
     } else {
-      //Serial.print("**ERR*: "); Serial.println(short (checksum));
       errorcount ++;
-      Serial.print("*err*");
     }
     framecount = framecount + 1;
     Serial.print(errorcount);    Serial.print("/");    Serial.println(framecount);
-    //delayMicroseconds(20);//上のシリアル表示しない場合、このディレイを入れる（タイミング調整用）
 
     //送信データを作成してセット
     checksum = 0;
-    for (int i = 0; i < MSG_SIZE - 1; i++) //配列の末尾以外をデータを入れる
+    for (int i = 0; i < MSG_SIZE - 1; i++) //配列の末尾以外にデータを入れる
     {
-      short rnd = random(-30000, 30000);
-      s_spi_meridim.sval[i] = rnd;
-      checksum += rnd; //チェックサムを加算
+      short rnd = random(-30000, 30000);//ここではランダムな配列を使う
+      s_spi_meridim.sval[i] = rnd;//配列にランダムなデータを入れる
+      checksum += int(s_spi_meridim.sval[i]); //チェックサムを加算
     }
-    s_spi_meridim.sval[MSG_SIZE - 1] = short(checksum & 0xFF ^ 0xFF); //データ末尾にチェックサムにする
-
-    for (int i = 0; i < MSG_BUFF - 1 ; i++) { //送信データの転記
+    checksum = short(~checksum);//チェックサムを確定
+    s_spi_meridim.sval[MSG_SIZE - 1] = short(checksum); //データ末尾にチェックサムを入れるでもよい
+    for (int i = 0; i < MSG_BUFF; i++) { //受信データのDMAから配列への転記
       s_spi_meridim_dma[i] = s_spi_meridim.bval[i] ;
     }
   }
